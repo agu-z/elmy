a0f476eff478309dd2a25c51bb08c2427d191a69
Agustín Zubiaga <sup@aguz.me>
2020-02-15 22:28:50 -0300

    [elm-ui] wrappedRow and column

--- packages/elm-ui/src/Element.elm
+++ packages/elm-ui/src/Element.elm
@@ -1,4 +1,4 @@
-module Element exposing (row, text)
+module Element exposing (column, row, text, wrappedRow)
 
 import UiAsm exposing (Attribute(..), Container(..), Element(..))
 
@@ -11,3 +11,13 @@ text =
 row : List (Attribute msg) -> List (Element msg) -> Element msg
 row =
     Container Row
+
+
+wrappedRow : List (Attribute msg) -> List (Element msg) -> Element msg
+wrappedRow =
+    Container WrappedRow
+
+
+column : List (Attribute msg) -> List (Element msg) -> Element msg
+column =
+    Container Column
